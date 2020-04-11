2c64a3803885bdf049d64add4d567979518f69f7
David Weis <davidmichaelweis@gmail.com>
2020-04-11 18:42:26 +0100

    Add mqtt adaptor

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,73 @@
 mod hopper_config;
+mod mqtt_adaptor;
 
+use clap::{App, Arg};
 use std::error::Error;
-use clap::{Arg, App};
 use std::path::Path;
+
+use log::*;
+use simplelog::*;
+use std::fs::OpenOptions;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let matches = App::new("Hopper body controller")
-                          .version("0.0.1")
-                          .author("David Weis <dweis7@gmail.com>")
-                          .about("Controls body of Hopper")
-                          .arg(Arg::with_name("body_config")
-                               .long("body_config")
-                               .help("Sets path to body config file (.yaml)")
-                               .required(true)
-                               .takes_value(true))
-                          .get_matches();
-    let body_config_path = Path::new(matches
-        .value_of("body_config")
-        .ok_or("body_config must be specified")?);
+        .version("0.0.1")
+        .author("David Weis <dweis7@gmail.com>")
+        .about("Controls body of Hopper")
+        .arg(
+            Arg::with_name("body_config")
+                .long("body_config")
+                .help("Sets path to body config file (.yaml)")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("mqtt_host")
+                .long("mqtt_host")
+                .help("MQTT hostname")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("log_path")
+                .long("log_path")
+                .help("Path for external log file.\nIf no give will only log to out")
+                .required(false)
+                .takes_value(true),
+        )
+        .get_matches();
 
-    let config = hopper_config::HopperConfig::load(body_config_path)?;
-    println!("{:?}", config);
+    if let Some(path) = matches.value_of("log_path") {
+        let log_file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(path)
+        .expect("Failed to open log file");
+        CombinedLogger::init(
+            vec![
+                TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed).expect("Failed to initialize logger"),
+                WriteLogger::new(LevelFilter::Info, Config::default(), log_file),
+            ]
+        ).expect("Failed to initialize logger");
+    } else {
+        CombinedLogger::init(
+            vec![
+                TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed).expect("Failed to initialize logger"),
+            ]
+        ).expect("Failed to initialize logger");
+    }
+
+    let body_config_path = Path::new(
+        matches
+            .value_of("body_config")
+            .ok_or("body_config must be specified")?,
+    );
+
+    let mqtt_host = matches.value_of("mqtt_host").unwrap_or("mqtt.local");
+
+    let _ = hopper_config::HopperConfig::load(body_config_path)?;
+
+    let mut mqtt = mqtt_adaptor::MqttAdaptor::new(&mqtt_host);
+    mqtt.send("Hi there!");
     Ok(())
 }
---
+++ src/mqtt_adaptor.rs
@@ -0,0 +1,79 @@
+use paho_mqtt as mqtt;
+use std::thread;
+use std::sync::mpsc::{ channel, Sender };
+use log::*;
+
+enum Command {
+    SendMessage(String),
+    Exit
+}
+
+pub struct MqttAdaptor {
+    sender: Sender<Command>,
+    join_handle: Option<thread::JoinHandle<()>>,
+}
+
+impl MqttAdaptor {
+    pub fn new(host: &str) -> MqttAdaptor {
+        let host = host.to_owned();
+        let (tx, rx): (Sender<Command>, _) = channel();
+        let mqtt_thread = thread::spawn(move || {
+            let client = match mqtt::Client::new(host) {
+                Ok(client) => client,
+                Err(_) => {
+                    error!("Failed to initialize MQTT client");
+                    return;
+                }
+            };
+
+            let connection_options = mqtt::ConnectOptionsBuilder::new()
+                .clean_session(true)
+                .finalize();
+            
+            match client.connect(connection_options) {
+                Ok(connection) => trace!("Connected to MQTT host {:?}", connection),
+                Err(error) => {
+                    error!("Failed to connect to MQTT host {}", error);
+                    return;
+                }
+            }
+
+            for message in rx.iter() {
+                match message {
+                    Command::SendMessage(message) => {
+                        if let Err(error) = client.publish(mqtt::Message::new("hopper/telemetry", message, 0)) {
+                            error!("Failed to send MQTT message {}", error);
+                        }
+                    },
+                    Command::Exit => break,
+                }
+            }
+        });
+        MqttAdaptor {
+            sender: tx,
+            join_handle: Some(mqtt_thread),
+        }
+    }
+
+    pub fn send(&mut self, message: &str) {
+        if let Err(error) = self.sender.send(Command::SendMessage(message.to_owned())) {
+            error!("Failed to load MQTT message to channel {}", error);
+        }
+    }
+}
+
+impl Drop for MqttAdaptor {
+    fn drop(&mut self) {
+        if let Err(error) = self.sender.send(Command::Exit) {
+            error!("Failed to load Exit message to channel {}", error);
+        }
+        match self.join_handle.take() {
+            Some(handle) => {
+                    if let Err(error) = handle.join() {
+                        error!("Failed joining MQTT thread with {:?}", error);
+                    }
+                },
+            None => error!("Missing join handle for MQTT thread"),
+        }
+    }
+}
