1217692b5787569d5d5ddfde18c8019143b1d2b9
David Weis <davidmicahelweis@gmail.com>
2020-04-26 19:11:41 +0100

    Filter logs

--- src/utilities.rs
+++ src/utilities.rs
@@ -15,16 +15,17 @@ use std::fs::OpenOptions;
 ///
 /// * `log_file` - Path to the optional file to log into
 pub fn start_loggers(log_file: Option<&str>) -> Result<(), Box<dyn Error>> {
+    let config = ConfigBuilder::new().add_filter_allow_str("hopper_rust::*").build();
     let mut loggers: Vec<Box<dyn SharedLogger>> = vec![];
     if let Some(path) = log_file {
         let log_file = OpenOptions::new().append(true).create(true).open(path)?;
-        let file_logger = WriteLogger::new(LevelFilter::Info, Config::default(), log_file);
+        let file_logger = WriteLogger::new(LevelFilter::Info, config.clone(), log_file);
         loggers.push(file_logger);
     }
-    match TermLogger::new(LevelFilter::Trace, Config::default(), TerminalMode::Mixed) {
+    match TermLogger::new(LevelFilter::Trace, config.clone(), TerminalMode::Mixed) {
         Some(logger) => loggers.push(logger as Box<dyn SharedLogger>),
         None => {
-            loggers.push(SimpleLogger::new(LevelFilter::Trace, Config::default()));
+            loggers.push(SimpleLogger::new(LevelFilter::Trace, config.clone()));
             warn!("Failed to create Term logger, creating simple logger instead");
         },
     }
