22be9c5adf1b71297c51888ebd8c76e0221a5558
David Weis <dweis7@gmail.com>
2020-12-27 02:52:43 +0000

    Compile with async version of dynamixel driver

--- src/body_controller/mod.rs
+++ src/body_controller/mod.rs
@@ -1,16 +1,18 @@
-pub mod motor_positions;
 mod motor_controller;
+pub mod motor_positions;
 
-pub use motor_positions::*;
-use motor_controller::*;
 use crate::hopper_config::BodyConfig;
 use crate::mqtt_adaptor::MqttAdaptor;
 use log::*;
-use looprate;
 use looprate::shared_timers::SharedTimer;
-use std::sync::{Arc, Mutex, mpsc::{channel, Sender, Receiver}};
+use motor_controller::*;
+pub use motor_positions::*;
+use std::sync::{
+    mpsc::{channel, Receiver, Sender},
+    Arc, Mutex,
+};
 use std::thread;
-
+use tokio::runtime::Runtime;
 
 enum BufferedCommand {
     SetCompliance(u8),
@@ -33,7 +35,7 @@ pub trait BodyController {
 }
 
 pub struct AsyncBodyController {
-    join_handle: Option<thread::JoinHandle<()>>,
+    join_handle: Option<thread::JoinHandle<anyhow::Result<()>>>,
     command: Arc<Mutex<Option<ImmediateCommand>>>,
     buffered_command_sender: Sender<BufferedCommand>,
     position_receiver: Receiver<Result<BodyMotorPositions, &'static str>>,
@@ -45,74 +47,87 @@ impl AsyncBodyController {
         body_config: BodyConfig,
         telemetry_sender: MqttAdaptor,
         mut monitor_timer: SharedTimer,
-    ) -> Box<dyn BodyController> {
+    ) -> Box<Self> {
         let (command_tx, command_rx) = channel();
         let (position_tx, position_rx) = channel();
-        let body_config = body_config.clone();
+        let body_config = body_config;
         let command = Arc::new(Mutex::new(None));
         let command_copy = Arc::clone(&command);
         let port_name = port.to_owned();
-        let join_handle = thread::spawn(move || {
-            let mut telemetry_rate = looprate::Rate::from_frequency(5.0);
+        let join_handle = thread::spawn(move || -> anyhow::Result<()> {
+            let mut rt = Runtime::new()?;
+            rt.block_on(async {
+                let mut telemetry_rate = looprate::Rate::from_frequency(5.0);
 
-            let mut loop_rate = looprate::Rate::from_frequency(50.0);
+                let mut loop_rate = looprate::Rate::from_frequency(50.0);
 
-            let mut motor_controller = MotorController::new(&port_name, body_config).unwrap();
+                let mut motor_controller = MotorController::new(&port_name, body_config).unwrap();
 
-            loop {
-                let command = command_copy.lock().unwrap().take();
-                monitor_timer.tick();
-                if let Some(command) = command {
-                    match command {
-                        ImmediateCommand::MoveToPosition(positions) => {
-                            if let Err(error) = motor_controller.move_to_position(positions) {
-                                warn!("Failed moving motors {}", error);
-                            }
-                        }
-                        ImmediateCommand::Exit => return,
-                    }
-                }
-                if telemetry_rate.check() {
-                    match motor_controller.read_mean_voltage() {
-                        Ok(mean) => telemetry_sender.send("voltage", &format!("{}", mean)),
-                        Err(error) => warn!("Failed to read voltage {}", error),
-                    }
-                    telemetry_sender.send("body_controller_rate", &monitor_timer.elapsed_hz().to_string())
-                }
-                if let Ok(buffered_command) = command_rx.try_recv() {
-                    match buffered_command {
-                        BufferedCommand::SetSpeed(speed) => {
-                            if let Err(error) = motor_controller.set_speed(speed) {
-                                warn!("Failed setting speed {}", error);
-                            }
-                        },
-                        BufferedCommand::SetCompliance(compliance) => {
-                            if let Err(error) = motor_controller.set_compliance(compliance) {
-                                warn!("Failed setting compliance {}", error);
-                            }
-                        },
-                        BufferedCommand::ReadPosition => {
-                            match motor_controller.read_positions() {
-                                Ok(motor_positions) => position_tx.send(Ok(motor_positions)).unwrap(),
-                                Err(error) => {
-                                    warn!("Failed reading motor positions {}", error);
-                                    position_tx.send(Err("Failed to read position")).unwrap()
+                loop {
+                    let command = command_copy.lock().unwrap().take();
+                    monitor_timer.tick();
+                    if let Some(command) = command {
+                        match command {
+                            ImmediateCommand::MoveToPosition(positions) => {
+                                if let Err(error) =
+                                    motor_controller.move_to_position(positions).await
+                                {
+                                    warn!("Failed moving motors {}", error);
                                 }
                             }
-                        },
-                        BufferedCommand::SetTorque(torque) => {
-                            if let Err(error) = motor_controller.set_torque(torque) {
-                                warn!("Failed setting torque {}", error);
+                            ImmediateCommand::Exit => return,
+                        }
+                    }
+                    if telemetry_rate.check() {
+                        match motor_controller.read_mean_voltage().await {
+                            Ok(mean) => telemetry_sender.send("voltage", &format!("{}", mean)),
+                            Err(error) => warn!("Failed to read voltage {}", error),
+                        }
+                        telemetry_sender.send(
+                            "body_controller_rate",
+                            &monitor_timer.elapsed_hz().to_string(),
+                        )
+                    }
+                    if let Ok(buffered_command) = command_rx.try_recv() {
+                        match buffered_command {
+                            BufferedCommand::SetSpeed(speed) => {
+                                if let Err(error) = motor_controller.set_speed(speed).await {
+                                    warn!("Failed setting speed {}", error);
+                                }
+                            }
+                            BufferedCommand::SetCompliance(compliance) => {
+                                if let Err(error) =
+                                    motor_controller.set_compliance(compliance).await
+                                {
+                                    warn!("Failed setting compliance {}", error);
+                                }
+                            }
+                            BufferedCommand::ReadPosition => {
+                                match motor_controller.read_positions().await {
+                                    Ok(motor_positions) => {
+                                        position_tx.send(Ok(motor_positions)).unwrap()
+                                    }
+                                    Err(error) => {
+                                        warn!("Failed reading motor positions {}", error);
+                                        position_tx.send(Err("Failed to read position")).unwrap()
+                                    }
+                                }
+                            }
+                            BufferedCommand::SetTorque(torque) => {
+                                if let Err(error) = motor_controller.set_torque(torque).await {
+                                    warn!("Failed setting torque {}", error);
+                                }
                             }
                         }
                     }
+                    loop_rate.wait()
                 }
-                loop_rate.wait()
-            }
+            });
+            Ok(())
         });
         Box::new(AsyncBodyController {
             join_handle: Some(join_handle),
-            command: command,
+            command,
             buffered_command_sender: command_tx,
             position_receiver: position_rx,
         })
@@ -121,7 +136,10 @@ impl AsyncBodyController {
 
 impl BodyController for AsyncBodyController {
     fn move_motors_to(&mut self, positions: BodyMotorPositions) {
-        let mut command = self.command.lock().expect("Body controller thread panicked");
+        let mut command = self
+            .command
+            .lock()
+            .expect("Body controller thread panicked");
         command.replace(ImmediateCommand::MoveToPosition(positions));
     }
 
@@ -153,10 +171,7 @@ impl BodyController for AsyncBodyController {
 
 impl Drop for AsyncBodyController {
     fn drop(&mut self) {
-        self.command
-            .lock()
-            .unwrap()
-            .replace(ImmediateCommand::Exit);
+        self.command.lock().unwrap().replace(ImmediateCommand::Exit);
         match self.join_handle.take() {
             Some(handle) => {
                 if let Err(error) = handle.join() {
--- src/body_controller/motor_controller.rs
+++ src/body_controller/motor_controller.rs
@@ -1,11 +1,9 @@
-
-use dynamixel_driver::*;
-use std::error::Error;
-use crate::hopper_config::{ BodyConfig, LegConfig };
 use super::motor_positions::*;
-use std::collections::VecDeque;
+use crate::hopper_config::{BodyConfig, LegConfig};
+use anyhow::Result;
+use dynamixel_driver::*;
 use log::*;
-
+use std::collections::VecDeque;
 
 pub struct MotorController {
     driver: DynamixelDriver,
@@ -15,7 +13,7 @@ pub struct MotorController {
 }
 
 impl MotorController {
-    pub fn new(port_name: &str, body_config: BodyConfig) -> Result<MotorController, Box<dyn Error>> {
+    pub fn new(port_name: &str, body_config: BodyConfig) -> Result<MotorController> {
         let dynamixel_driver = DynamixelDriver::new(port_name)?;
         Ok(MotorController {
             driver: dynamixel_driver,
@@ -25,55 +23,52 @@ impl MotorController {
         })
     }
 
-    pub fn move_to_position(&mut self, positions: BodyMotorPositions) -> Result<(), Box<dyn Error>> {
-        let commands = create_commands_for_body(
-            &self.body_config,
-            &positions,
-        );
-        self.driver.sync_write_position_rad(commands)?;
+    pub async fn move_to_position(&mut self, positions: BodyMotorPositions) -> Result<()> {
+        let commands = create_commands_for_body(&self.body_config, &positions);
+        self.driver.sync_write_position_rad(commands).await?;
         Ok(())
     }
 
-    pub fn set_speed(&mut self, speed: u16) -> Result<(), Box<dyn Error>> {
+    pub async fn set_speed(&mut self, speed: u16) -> Result<()> {
         let commands = self
             .body_config
             .get_ids()
             .iter()
             .map(|id| SyncCommand::new(*id, speed as u32))
             .collect::<Vec<_>>();
-        self.driver.sync_write_moving_speed(commands)?;
+        self.driver.sync_write_moving_speed(commands).await?;
         Ok(())
     }
 
-    pub fn set_compliance(&mut self, compliance: u8) -> Result<(), Box<dyn Error>> {
+    pub async fn set_compliance(&mut self, compliance: u8) -> Result<()> {
         let commands = self
             .body_config
             .get_ids()
             .iter()
             .map(|id| SyncCommand::new(*id, compliance as u32))
             .collect::<Vec<_>>();
-        self.driver.sync_write_compliance_both(commands)?;
+        self.driver.sync_write_compliance_both(commands).await?;
         Ok(())
     }
 
-    pub fn set_torque(&mut self, torque: bool) -> Result<(), Box<dyn Error>> {
+    pub async fn set_torque(&mut self, torque: bool) -> Result<()> {
         let commands = self
             .body_config
             .get_ids()
             .iter()
             .map(|id| (*id, torque))
             .collect::<Vec<_>>();
-        self.driver.sync_write_torque(commands)?;
+        self.driver.sync_write_torque(commands).await?;
         Ok(())
     }
 
-    pub fn read_mean_voltage(&mut self) -> Result<f32, Box<dyn Error>> {
+    pub async fn read_mean_voltage(&mut self) -> Result<f32> {
         let ids = self.body_config.get_ids();
         // Load all on first call
         if self.last_voltages.is_empty() {
             info!("Read all voltages");
             for id in &ids {
-                let voltage = self.driver.read_voltage(*id)?;
+                let voltage = self.driver.read_voltage(*id).await?;
                 self.last_voltages.push_front(voltage);
             }
             self.last_read_voltage = ids.len() - 1;
@@ -83,7 +78,7 @@ impl MotorController {
             self.last_read_voltage = 0;
         }
         let id = ids[self.last_read_voltage];
-        let voltage = self.driver.read_voltage(id)?;
+        let voltage = self.driver.read_voltage(id).await?;
         self.last_voltages.push_front(voltage);
         self.last_voltages.pop_back();
         let sum: f32 = self.last_voltages.iter().sum();
@@ -91,23 +86,23 @@ impl MotorController {
         Ok(mean)
     }
 
-    pub fn read_positions(&mut self) -> Result<BodyMotorPositions, Box<dyn Error>> {
-        fn read_leg_positions(
+    pub async fn read_positions(&mut self) -> Result<BodyMotorPositions> {
+        async fn read_leg_positions(
             driver: &mut DynamixelDriver,
-            leg_config: &LegConfig
-        ) -> Result<LegMotorPositions, Box<dyn Error>> {
-            let coxa = driver.read_position_rad(leg_config.coxa_id)?;
-            let femur = driver.read_position_rad(leg_config.femur_id)?;
-            let tibia = driver.read_position_rad(leg_config.tibia_id)?;
+            leg_config: &LegConfig,
+        ) -> Result<LegMotorPositions> {
+            let coxa = driver.read_position_rad(leg_config.coxa_id).await?;
+            let femur = driver.read_position_rad(leg_config.femur_id).await?;
+            let tibia = driver.read_position_rad(leg_config.tibia_id).await?;
             Ok(LegMotorPositions::new(coxa, femur, tibia))
         }
         Ok(BodyMotorPositions::new(
-            read_leg_positions(&mut self.driver, &self.body_config.left_front)?,
-            read_leg_positions(&mut self.driver, &self.body_config.left_middle)?,
-            read_leg_positions(&mut self.driver, &self.body_config.left_rear)?,
-            read_leg_positions(&mut self.driver, &self.body_config.right_front)?,
-            read_leg_positions(&mut self.driver, &self.body_config.right_middle)?,
-            read_leg_positions(&mut self.driver, &self.body_config.right_rear)?,
+            read_leg_positions(&mut self.driver, &self.body_config.left_front).await?,
+            read_leg_positions(&mut self.driver, &self.body_config.left_middle).await?,
+            read_leg_positions(&mut self.driver, &self.body_config.left_rear).await?,
+            read_leg_positions(&mut self.driver, &self.body_config.right_front).await?,
+            read_leg_positions(&mut self.driver, &self.body_config.right_middle).await?,
+            read_leg_positions(&mut self.driver, &self.body_config.right_rear).await?,
         ))
     }
 }
