7e3bdc7997ed06c18bef73f2c7f9bf0536884358
David Weis <dweis7@gmail.com>
2020-12-29 13:02:55 +0000

    Start implmenting sinusoid stepping

--- src/motion_controller/mod.rs
+++ src/motion_controller/mod.rs
@@ -1,5 +1,10 @@
-use crate::ik_controller::{leg_positions::MoveTowards, IkControlable};
+use crate::ik_controller::{
+    leg_positions::{LegPositions, MoveTowards},
+    IkControlable,
+};
 use anyhow::Result;
+use nalgebra::{distance, Point3};
+use std::f32;
 use std::time::Duration;
 use tokio::time;
 use tokio::{spawn, task::JoinHandle};
@@ -42,4 +47,67 @@ impl MotionController {
         }
         Ok(())
     }
+}
+
+#[allow(dead_code)]
+enum Tripod {
+    LRL,
+    RLR,
+}
+
+#[allow(dead_code)]
+struct StepIterator {
+    start: LegPositions,
+    current: LegPositions,
+    target: LegPositions,
+    max_move: f32,
+    step_height: f32,
+    tripod: Tripod,
+}
+
+impl Iterator for StepIterator {
+    type Item = LegPositions;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        None
+    }
+}
+
+#[allow(dead_code)]
+fn step_lifted_leg(
+    start: &Point3<f32>,
+    last_written: &Point3<f32>,
+    target: &Point3<f32>,
+    max_move: f32,
+    step_height: f32,
+) -> (Point3<f32>, bool) {
+    if last_written == target {
+        return (*target, false);
+    }
+    if distance(&last_written.xy(), &target.xy()) <= max_move {
+        return (*target, true);
+    }
+    let full_ground_translation = target.xy() - start.xy();
+    let step_translation = last_written.xy() + full_ground_translation.normalize() * max_move;
+    let progress = full_ground_translation.magnitude() / full_ground_translation.magnitude();
+    let height = (progress * f32::consts::PI).sin() * step_height + start.z;
+    let ground_position = start.xy() + step_translation.coords;
+    let new_position = Point3::new(ground_position.x, ground_position.y, height);
+    (new_position, true)
+}
+
+#[allow(dead_code)]
+fn step_grounded_leg(
+    _start: &Point3<f32>,
+    last_written: &Point3<f32>,
+    target: &Point3<f32>,
+    max_move: f32,
+) -> (Point3<f32>, bool) {
+    if last_written == target {
+        return (*target, false);
+    }
+    if distance(&last_written.xy(), &target.xy()) <= max_move {
+        return (*target, true);
+    }
+    unimplemented!("Not yet");
 }
