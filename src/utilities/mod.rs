9eea8d4e4008cef6a153c23df995e1d77426df2e
David Weis <dweis7@gmail.com>
2020-12-31 08:13:08 +0000

    Fix logging

--- src/utilities/mod.rs
+++ src/utilities/mod.rs
@@ -15,23 +15,22 @@ use std::fs::OpenOptions;
 ///
 /// * `log_file` - Path to the optional file to log into
 pub fn start_loggers(log_file: Option<String>, verbosity_level: u8) -> Result<()> {
-    let config = Config::default();
+    let config = ConfigBuilder::new().add_filter_ignore_str("gilrs").build();
     let filter = match verbosity_level {
-        0 => LevelFilter::Info,
+        0 => LevelFilter::Warn,
+        1 => LevelFilter::Info,
+        2 => LevelFilter::Debug,
+        3 => LevelFilter::Trace,
         _ => LevelFilter::max(),
     };
     let mut loggers: Vec<Box<dyn SharedLogger>> = vec![];
     if let Some(path) = log_file {
         let log_file = OpenOptions::new().append(true).create(true).open(path)?;
-        let file_logger = WriteLogger::new(filter, config, log_file);
+        let file_logger = WriteLogger::new(filter, config.clone(), log_file);
         loggers.push(file_logger);
     }
-    // loggers.push(TermLogger::new(filter, config, TerminalMode::Mixed));
-    loggers.push(TermLogger::new(
-        filter,
-        Config::default(),
-        TerminalMode::Mixed,
-    ));
+    loggers.push(TermLogger::new(filter, config, TerminalMode::Mixed));
     CombinedLogger::init(loggers)?;
+    info!("Logging level set to {}", filter);
     Ok(())
 }
