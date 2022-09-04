30dc5931248260c431a9243843962ecdc2f260ce
David Weis <davidmichaelweis@gmail.com>
2022-09-04 23:39:18 +0100

    Revert unwanted changes

--- src/motion_controller/mod.rs
+++ src/motion_controller/mod.rs
@@ -105,12 +105,12 @@ enum BlockingCommand {
 
 const TICK_DURATION: Duration = Duration::from_millis(1000 / 50);
 const MAX_MOVE: f32 = 0.001;
-const MAX_TRANSLATION_STEP: f32 = 0.004;
+const MAX_TRANSLATION_STEP: f32 = 0.005;
 const MAX_ROTATION_STEP: f32 = std::f32::consts::PI / 180.0;
 const STEP_HEIGHT: f32 = 0.03;
 const GROUNDED_STEP_HEIGHT: f32 = -0.0;
 const VOLTAGE_READ_PERIOD: Duration = Duration::from_millis(200);
-const MOVE_DURATION: Duration = Duration::from_millis(700);
+const MOVE_DURATION: Duration = Duration::from_millis(400);
 
 struct MotionControllerLoop {
     ik_controller: Box<dyn IkControllable>,
