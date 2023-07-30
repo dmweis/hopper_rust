505677aa6b9dac1498dcbb13b5fa1c8099680e6d
David Weis <dweis7@gmail.com>
2023-07-30 02:04:02 +0100

    Test

--- src/bin/hopper.rs
+++ src/bin/hopper.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 use hopper_rust::{
     body_controller, body_controller::BodyController, camera::start_camera,
     configuration::get_configuration, error::HopperError, hopper_body_config, ik_controller,
-    lidar::start_lidar_driver, motion_controller, speech::SpeechService, utilities,
+    lidar::start_lidar_driver, motion_controller, speech::SpeechService, udp_remote, utilities,
     zenoh_remote::simple_zenoh_controller,
 };
 use std::{
@@ -87,11 +87,11 @@ async fn main() -> Result<()> {
 
     start_camera(zenoh_session.clone(), &app_config.camera).await?;
 
-    // udp_remote::udp_controller_handler(&mut motion_controller)
-    //     .await
-    //     .unwrap();
+    udp_remote::udp_controller_handler(&mut motion_controller)
+        .await
+        .unwrap();
 
-    simple_zenoh_controller(&mut motion_controller, zenoh_session.clone()).await?;
+    // simple_zenoh_controller(&mut motion_controller, zenoh_session.clone()).await?;
 
     motion_controller.set_body_state(motion_controller::BodyState::Grounded);
     tokio::time::sleep(Duration::from_secs_f32(2.0)).await;
