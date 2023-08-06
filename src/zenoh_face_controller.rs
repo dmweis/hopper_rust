499b35fefb12686b3d2ef0f14f99445afca3e4eb
David Weis <dweis7@gmail.com>
2023-08-06 15:36:55 +0100

    Add more color and animation support

--- src/zenoh_face_controller.rs
+++ src/zenoh_face_controller.rs
@@ -24,6 +24,28 @@ pub async fn start_face_controller(
 
     tokio::spawn(async move {
         let mut selected_color = hopper_face::driver::OFF;
+        let mut selected_animation = String::from("larson_scanner");
+
+        fn set_animation(
+            face_controller: &FaceController,
+            animation: &str,
+            color: hopper_face::driver::RGB,
+        ) -> anyhow::Result<()> {
+            match animation.to_lowercase().as_str() {
+                "larson_scanner" => face_controller.larson_scanner(color)?,
+                "run_animation" => face_controller.run_animation(color)?,
+                "off" => face_controller.off()?,
+                "cycle_all_colors" => face_controller.cycle_all_colors()?,
+                "cycle_bright_colors" => face_controller.cycle_bright_colors()?,
+                "cycle_normal_colors" => face_controller.cycle_normal_colors()?,
+                "count_down_basic" => face_controller.count_down_basic()?,
+                "breathing" => face_controller.breathing(color)?,
+                "solid_color" => face_controller.solid_color(color)?,
+                _ => error!("Unknown animation {}", animation),
+            }
+            Ok(())
+        }
+
         loop {
             let res: anyhow::Result<()> = async {
                 select! {
@@ -39,14 +61,13 @@ pub async fn start_face_controller(
                             "off" => selected_color = hopper_face::driver::OFF,
                             _ => error!("Unknown color {}", color),
                         }
+                        set_animation(&face_controller, &selected_animation, selected_color)?;
                     }
                     animation = face_animation_subscriber.recv_async() => {
                         let animation = animation?;
                         let animation: String = animation.value.try_into()?;
-                        match animation.to_lowercase().as_str() {
-                            "larson_scanner" => face_controller.larson_scanner(selected_color)?,
-                            _ => error!("Unknown animation {}", animation),
-                        }
+                        selected_animation = animation;
+                        set_animation(&face_controller, &selected_animation, selected_color)?;
                     }
                 }
                 Ok(())
